9c341d1f6d76729d7b5b26f9c8459a1704edf80d
osha007 <137976522+osha007@users.noreply.github.com>
2023-06-28 09:47:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['94779996005'] //ur owner number
+global.ownernomer = "94779996005" //ur owner number2
+global.ownername = "B2r" //ur owner name
+global.ytname = "YT: real.b2r" //ur yt chanel name
+global.socialm = "GitHub: osha007" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "B2R"
+global.ownernumber = '9477996005'
+global.ownername = 'KAVISHKA LAKSHAN'
+global.ownerNumber = ["94779996005@s.whatsapp.net"]
+global.ownerweb = "https://www.youtube.com/channel/UCLySJmD-ILKNs5Rwwfoyn2g"
+global.websitex = "https://www.youtube.com/channel/UCLySJmD-ILKNs5Rwwfoyn2g"
+global.wagc = "https://chat.whatsapp.com"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "REAL B2R INC."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "kaishka\n\n+9477996005"
+global.creator = "9477996005@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
